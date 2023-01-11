5654567a90c9c5d8a89aef42d599d6371ee77902
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-11 18:34:12 +0200

    add les1 task5

---
+++ lesson-1/todo-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElem = document.querySelector('#root');
+
+const titleElem = <div className="title">Todo List</div>;
+
+ReactDOM.render(titleElem, rootElem);
