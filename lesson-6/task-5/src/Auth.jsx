c5f566e585d6f52bf41e1f918c86cf2f5473d056
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-16 20:43:20 +0200

    add les6 t5

---
+++ lesson-6/task-5/src/Auth.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Login from './Login';
+import Logout from './Logout';
+import Spinner from './Spinner';
+
+class Auth extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLogin: false,
+      spinner: false,
+    };
+  }
+
+  loginButton = () => {
+    this.setState({
+      spinner: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        isLogin: true,
+        spinner: false,
+      });
+    }, 2000);
+  };
+
+  logoutButton = () => {
+    this.setState({
+      isLogin: false,
+    });
+  };
+
+  render() {
+    let button;
+    if (this.state.isLogin) {
+      button = <Logout onLogout={this.logoutButton} />;
+    } else {
+      button =
+        this.state.spinner === true ? (
+          <Spinner size={50} />
+        ) : (
+          <Login onLogin={this.loginButton} />
+        );
+    }
+    return <>{button}</>;
+  }
+}
+
+export default Auth;
---
+++ lesson-6/task-5/src/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = (props) => {
+  return (
+    <span
+      style={{ width: props.size, height: props.size }}
+      className="spinner"
+    ></span>
+  );
+};
+
+export default Spinner;
