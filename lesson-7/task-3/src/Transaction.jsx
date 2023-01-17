49eb4c964bbafcd64a123f421ef98860c601ccb4
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-17 19:50:24 +0200

    change formatter

--- lesson-7/task-3/src/Transaction.jsx
+++ lesson-7/task-3/src/Transaction.jsx
@@ -3,8 +3,9 @@ import moment from 'moment';
 
 const date = (date) => moment(date).format('DD MMM');
 const dateTime = (time) => moment(time).format('HH:mm');
-const rightNumbers = (numbers) =>
-  new Intl.NumberFormat('en-GB').format(numbers);
+// const rightNumbers = (numbers) =>
+//   new Intl.NumberFormat('en-GB').format(numbers);
+const formatter = new Intl.NumberFormat('en-GB');
 
 const Transaction = ({ from, to, amount, rate, time }) => {
   return (
@@ -12,8 +13,8 @@ const Transaction = ({ from, to, amount, rate, time }) => {
       <span className="transaction__date">{date(time)}</span>
       <span className="transaction__time">{dateTime(time)}</span>
       <span className="transaction__assets">{`${from} → ${to}`} </span>
-      <span className="transaction__rate">{rightNumbers(rate)}</span>
-      <span className="transaction__amount">{rightNumbers(amount)}</span>
+      <span className="transaction__rate">{rate}</span>
+      <span className="transaction__amount">{formatter.format(amount)}</span>
     </li>
   );
 };
