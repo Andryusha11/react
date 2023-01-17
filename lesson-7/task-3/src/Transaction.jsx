e1a91910890c1f8258689c8c23ad9d9d78dcf72f
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-17 19:53:42 +0200

    мфдгу

--- lesson-7/task-3/src/Transaction.jsx
+++ lesson-7/task-3/src/Transaction.jsx
@@ -12,7 +12,7 @@ const Transaction = ({ from, to, amount, rate, time }) => {
     <li className="transaction">
       <span className="transaction__date">{date(time)}</span>
       <span className="transaction__time">{dateTime(time)}</span>
-      <span className="transaction__assets">{`${from} → ${to}`} </span>
+      <span className="transaction__assets">{`${from} → ${to}`}</span>
       <span className="transaction__rate">{rate}</span>
       <span className="transaction__amount">{formatter.format(amount)}</span>
     </li>
