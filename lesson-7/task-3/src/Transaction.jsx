531d92b74e14414de3b9d31248e10dae04fd9f4c
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-17 19:07:04 +0200

    add les7 t3

---
+++ lesson-7/task-3/src/Transaction.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import moment from 'moment';
+
+const date = moment().format('DD MMM');
+const dateTime = (time) => moment(time).format('hh:mm');
+
+const Transaction = ({ from, to, amount, rate, time }) => {
+  return (
+    <li className="transaction">
+      <span className="transaction__date">{date}</span>
+      <span className="transaction__time">{dateTime(time)}</span>
+      <span className="transaction__assets">{`${from} ➡ ${to}`} </span>
+      <span className="transaction__rate">{rate}</span>
+      <span className="transaction__amount">{amount}</span>
+    </li>
+  );
+};
+
+export default Transaction;
---
+++ lesson-7/task-3/src/TransactionsList.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Transaction from './Transaction';
+
+class TransactionsList extends Component {
+  render() {
+    return (
+      <ul className="transactions">
+        {this.props.transactions.map((transaction) => (
+          <Transaction
+            key={transaction.id}
+            to={transaction.to}
+            amount={transaction.amount}
+            rate={transaction.rate}
+            time={transaction.time}
+            from={transaction.from}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TransactionsList;
