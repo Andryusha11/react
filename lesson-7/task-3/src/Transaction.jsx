50fc88faf3180f50787feffe463ccd1685ecc964
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-17 19:14:15 +0200

    change format of number

--- lesson-7/task-3/src/Transaction.jsx
+++ lesson-7/task-3/src/Transaction.jsx
@@ -3,6 +3,7 @@ import moment from 'moment';
 
 const date = moment().format('DD MMM');
 const dateTime = (time) => moment(time).format('hh:mm');
+const rightAmount = (amount) => new Intl.NumberFormat('en-GB').format(amount);
 
 const Transaction = ({ from, to, amount, rate, time }) => {
   return (
@@ -11,7 +12,7 @@ const Transaction = ({ from, to, amount, rate, time }) => {
       <span className="transaction__time">{dateTime(time)}</span>
       <span className="transaction__assets">{`${from} ➡ ${to}`} </span>
       <span className="transaction__rate">{rate}</span>
-      <span className="transaction__amount">{amount}</span>
+      <span className="transaction__amount">{rightAmount(amount)}</span>
     </li>
   );
 };
