07e9b5cab9372df53e034b772c2ab5c3f928ba73
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-20 13:43:27 +0200

    remove console.log

--- lesson-8/task-1/src/Life.jsx
+++ lesson-8/task-1/src/Life.jsx
@@ -20,7 +20,6 @@ class Life extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log(prevProps);
     console.log(
       'componentDidUpdate(prevProps, prevState): some updates based on new props'
     );
