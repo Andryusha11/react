b14f0d377225d1dd2d0ae2744e6283f1d96f9485
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-26 09:08:43 +0200

    add les10 t3

---
+++ lesson-10/task-3/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './Calculator';
+import './index.scss';
+
+const root = document.getElementById('root');
+ReactDOM.render(<Calculator />, root);
