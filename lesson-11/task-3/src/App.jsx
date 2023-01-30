db9719b768956f6606287e5a05915cb93ff5e500
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-30 13:18:22 +0200

    change state and components

--- lesson-11/task-3/src/App.jsx
+++ lesson-11/task-3/src/App.jsx
@@ -1,41 +1,18 @@
 import React, { Component } from 'react';
 import Expand from './Expand';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-class App extends Component {
-  state = {
-    isContent: false,
-  };
-
-  hideContent = () => {
-    this.setState({ isContent: false });
-  };
-  showContent = () => {
-    this.setState({ isContent: true });
-  };
-
-  render() {
-    const icon = this.state.isContent ? (
-      <FontAwesomeIcon icon={faChevronUp} onClick={this.hideContent} />
-    ) : (
-      <FontAwesomeIcon icon={faChevronDown} onClick={this.showContent} />
-    );
-
-    const expandChildElem = this.state.isContent ? (
-      <p>
-        Hooks are a new addition in React 16.8. They let you use state and other
-        React features without writing a class.
-      </p>
-    ) : null;
-
-    return (
-      <div className="app">
-        <Expand icon={icon} title={'Some title'}>
-          <div className="expand__content">{expandChildElem}</div>
-        </Expand>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <Expand title={'Some title'}>
+        <div className="expand__content">
+          <p>
+            Hooks are a new addition in React 16.8. They let you use state and
+            other React features without writing a class.
+          </p>
+        </div>
+      </Expand>
+    </div>
+  );
+};
 export default App;
--- lesson-11/task-3/src/Expand.jsx
+++ lesson-11/task-3/src/Expand.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-const Expand = ({ children, icon, title }) => {
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn">{icon}</button>
+class Expand extends Component {
+  state = {
+    isContent: false,
+  };
+
+  hideContent = () => {
+    this.setState({ isContent: false });
+  };
+  showContent = () => {
+    this.setState({ isContent: true });
+  };
+  render() {
+    const { children, title } = this.props;
+    const icon = this.state.isContent ? (
+      <FontAwesomeIcon icon={faChevronUp} onClick={this.hideContent} />
+    ) : (
+      <FontAwesomeIcon icon={faChevronDown} onClick={this.showContent} />
+    );
+    const childElem = this.state.isContent ? children : null;
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn">{icon}</button>
+        </div>
+        {childElem}
       </div>
-      {children}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Expand;
