5ab6301c2033c747798b811b137f82801fcfe3ab
Andrew Valchuk <andryushahach1999@gmail.com>
2023-01-30 13:34:14 +0200

    ..

--- lesson-11/task-3/src/App.jsx
+++ lesson-11/task-3/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Expand from './Expand';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
   return (
