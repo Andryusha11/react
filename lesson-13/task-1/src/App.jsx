19719dc00efd11c65a605533a15ed26fb15c452d
Andrew Valchuk <andryushahach1999@gmail.com>
2023-02-05 14:20:32 +0200

    add les13 t1

---
+++ lesson-13/task-1/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Switch, Link, Route } from 'react-router-dom';
+import Contacts from './Contacts';
+import Home from './Home';
+import PageNotFound from './PageNotFound';
+import Products from './Products';
+
+const App = () => {
+  return (
+    <>
+      <div className="page">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/products">
+              <Products />
+            </Route>
+            <Route path="/contacts">
+              <Contacts />
+            </Route>
+            <Route path="*">
+              <PageNotFound />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </>
+  );
+};
+
+export default App;
