23c4277fda768706794242368f267024dfcefbb3
Andrew Valchuk <andryushahach1999@gmail.com>
2023-02-08 18:35:30 +0200

    add les13 t3

---
+++ lesson-13/task-3/src/Users.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+import User from './User';
+
+const Products = () => {
+  return (
+    <div className="page__content">
+      <h1>Users</h1>
+      <ul className="navigation">
+        <li className="navigation__item">
+          <Link to="/users/github">Github</Link>
+        </li>
+        <li className="navigation__item">
+          <Link to="/users/facebook">Facebook</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/users">
+          <span>Select a product please</span>
+        </Route>
+        <Route path="/users/:userId" component={User}></Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default Products;
