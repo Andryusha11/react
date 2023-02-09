6ac40637f37dad9926e0294eca01114fea780b6f
Andrew Valchuk <andryushahach1999@gmail.com>
2023-02-09 16:13:42 +0200

    add checking states

--- lesson-13/task-3/src/User.jsx
+++ lesson-13/task-3/src/User.jsx
@@ -1,46 +1,51 @@
 import React, { Component } from 'react';
 
 class User extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-    };
-  }
+  state = {
+    user: null,
+  };
 
   componentDidMount = () => {
-    this.fetchUser(this.props.match.params.userId);
-    console.log(this.props.match.params.userId);
-    console.log(this.state.user);
+    this.fetchUser();
   };
 
   componentDidUpdate = (prevProps) => {
     if (prevProps.match.params.userId !== this.props.match.params.userId) {
-      this.fetchUser(this.props.match.params.userId);
+      this.fetchUser();
     }
   };
 
-  fetchUser = (userId) => {
-    fetch(`https://api.github.com/users/${userId}`)
-      .then((response) => response.json())
-      .then((data) =>
+  fetchUser = () => {
+    const { match } = this.props;
+    fetch(`https://api.github.com/users/${match.params.userId}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error();
+      })
+      .then((data) => {
         this.setState({
           user: data,
-        })
-      );
+        });
+      });
   };
 
   render() {
+    const { user } = this.state;
+    if (!user) {
+      return null;
+    }
     return (
       <div className="user">
         <img
           alt="User Avatar"
-          src={this.state.user?.avatar_url}
+          src={this.state.user.avatar_url}
           className="user__avatar"
         />
         <div className="user__info">
-          <span className="user__name">{this.state.user?.name}</span>
-          <span className="user__location">{this.state.user?.location}</span>
+          <span className="user__name">{this.state.user.name}</span>
+          <span className="user__location">{this.state.user.location}</span>
         </div>
       </div>
     );
