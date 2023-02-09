9637427c9ea4bd14790b91342182267304e51b84
Andrew Valchuk <andryushahach1999@gmail.com>
2023-02-09 17:40:00 +0200

    add les14 t1

---
+++ lesson-14/task-1/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+import './index.scss';
+
+const root = document.getElementById('root');
+ReactDOM.render(<Counter />, root);
