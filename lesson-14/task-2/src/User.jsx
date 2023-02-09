08c65cab3c7ca6b25593abe5abfa79a1ae96c72e
Andrew Valchuk <andryushahach1999@gmail.com>
2023-02-09 18:51:09 +0200

    add les14 t2

---
+++ lesson-14/task-2/src/User.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const User = () => {
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error();
+      })
+      .then((data) => {
+        setUser(data);
+      });
+  }, [userId]);
+  if (!user) {
+    return null;
+  }
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={user.avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{user.name}</span>
+        <span className="user__location">{user.location}</span>
+      </div>
+    </div>
+  );
+};
+
+export default User;
