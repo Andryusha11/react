20c484661c27b8c3550521e6ceca4acc0d8505ee
Andrew Valchuk <andryushahach1999@gmail.com>
2023-02-10 16:59:54 +0200

    add les14 t3

---
+++ lesson-14/task-3/src/Dimensions.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+const Dimensions = () => {
+  const [demenions, setDimensions] = useState({ width: null, height: null });
+  useEffect(() => {
+    const { innerHeight, innerWidth } = window;
+    setDimensions({
+      width: innerWidth,
+      height: innerHeight,
+    });
+    const handleResize = (e) => {
+      const { innerHeight, innerWidth } = e.target;
+      setDimensions({
+        width: innerWidth,
+        height: innerHeight,
+      });
+    };
+    addEventListener('resize', handleResize);
+  }, []);
+  const { width, height } = demenions;
+  return <div className="dimensions">{`${width} - ${height}`}</div>;
+};
+
+export default Dimensions;
